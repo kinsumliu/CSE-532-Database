aa56d3a32553c22122ef689e9bd15695ec24d2d3
Kin Sum Liu <kinsumliu@gmail.com>
2014-10-28 21:25:41 -0400

    Create schema and data queries

--- project2/tss_data.sql
+++ project2/tss_data.sql
@@ -1,1 +1,7 @@
-INSERT INTO Contestants (name) VALUES ('Joe');
+INSERT INTO Contestants (name) VALUES ('Joe'), ('Bob'), ('Mary'), ('Ann'), ('Bess'), ('Tom'), ('Don');
+INSERT INTO Pieces (name) VALUES ('Barcarolle'), ('Giselle'), ('Besame Mucho'), ('Swan Lake'), ('Habanera'), ('The Tramp');
+INSERT INTO Judges (name) VALUES ('Judy'), ('Lucy'), ('Irving'), ('Phil'), ('Oscar');
+
+-- any all http://www.postgresql.org/docs/9.1/static/arrays.html
+-- http://stackoverflow.com/questions/11957925/psycopg2-mapping-python-list-of-dicts-to-postgres-array-of-composite-type
+INSERT INTO Performances VALUES ('Joe', 'Barcarolle',  ARRAY[('Judy', 7), ('Lucy', 8), ('Irving', 6)]::ResultType[]);
--- project2/tss_schema.sql
+++ project2/tss_schema.sql
@@ -1,25 +1,54 @@
 CREATE TYPE PersonType AS (
-	name CHAR(20)
+	name text
 );
 
 CREATE TYPE PieceType AS (
-	name CHAR(20)
+	name text
 );
 
+CREATE TYPE ResultType AS (
+	judge text,
+	score integer
+);
+
+-- CREATE TYPE PerformanceType AS (
+-- 	contestant REF(PersonType) SCOPE Contestants
+--	piece REF(PieceType) SCOPE Pieces
+-- 	results ROW(judge REF(PersonType) SCOPE Judges, score integer CHECK(score > 0) ) MULTISET
+-- );
 CREATE TYPE PerformanceType AS (
-	contestant REF(PersonType) SCOPE Contestants
-	judgements ROW(judge REF(PersonType) SCOPE Judges, score integer ) MULTISET
+	contestant text,
+	piece text,
+	results ResultType[]
 );
 
-CREATE TYPE ShowType AS (
-	showdate date,
-	performances REF(PerformanceType) SCOPE Performances MULTISET
+-- CREATE TYPE ShowType AS (
+-- 	showdate date,
+-- 	performances REF(PerformanceType) SCOPE Performances MULTISET
+-- );
+
+-- CREATE TYPE ShowType AS (
+-- 	showdate date,
+-- 	performances PerformanceType[]
+-- );
+
+CREATE TABLE Contestants OF PersonType (
+	UNIQUE(name),
+	PRIMARY KEY(name)
 );
 
-CREATE TABLE Contestants OF PersonType;
+CREATE TABLE Judges OF PersonType (
+	UNIQUE(name),
+	PRIMARY KEY(name)
+);
 
-CREATE TABLE Judges OF PersonType;
+CREATE TABLE Pieces OF PieceType(
+	UNIQUE(name),
+	PRIMARY KEY(name)
+);
 
-CREATE TABLE Performances OF PerformanceType;
-
-CREATE TABLE Shows OF ShowType;
+CREATE TABLE Performances OF PerformanceType (
+	FOREIGN KEY (contestant) REFERENCES Contestants,
+	FOREIGN KEY (piece) REFERENCES Pieces
+);
+-- CREATE TABLE Shows OF ShowType;
