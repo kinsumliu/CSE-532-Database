efedfd10ff0984cd762049d331c169095d84932f
Rey <rey123123.cs98g@g2.nctu.edu.tw>
2014-11-01 15:33:27 -0400

    update q5

--- project2/tss.sql
+++ project2/tss.sql
@@ -1,19 +1,22 @@
 -- Query 1: Find all pairs of contestants who happened to audition the same piece
 -- during the same show and got the same score from at least one judge
 
-SELECT C1.name, C2.name
-FROM Contestants C1, Contestants C2,
-  Show_Performances SP1, Show_Performances SP2,
-  Performances P1, Performances P2
-WHERE SP1.show = SP2.show AND
-  SP1.performance = P1.oid AND
-  SP2.performance = P2.oid AND
-  P1.piece = P2.piece AND
-  P1.contestant = C1.oid AND
-  P2.contestant = C2.oid AND
-  C1.name < C2.name AND
-  P1.results && P2.results
-;
+CREATE VIEW Direct_Coaudition AS (
+	SELECT C1.name AS name1, C2.name AS name2
+	FROM Contestants C1, Contestants C2,
+	  Show_Performances SP1, Show_Performances SP2,
+	  Performances P1, Performances P2
+	WHERE SP1.show = SP2.show AND
+	  SP1.performance = P1.oid AND
+	  SP2.performance = P2.oid AND
+	  P1.piece = P2.piece AND
+	  P1.contestant = C1.oid AND
+	  P2.contestant = C2.oid AND
+	  C1.name != C2.name AND
+	  P1.results && P2.results
+);
+
+SELECT * FROM Direct_Coaudition WHERE name1 < name2;
 
 -- Query 2: Find all pairs of contestants who happened to audition the same piece
 -- (in possibly different shows) and got the same average score for that piece.
@@ -102,3 +105,18 @@ WHERE
 --     WHERE
 -- )
 -- SELECT * FROM coaudition;
+
+CREATE RECURSIVE VIEW Coaudition (name1, name2) AS (
+	SELECT *
+	FROM Direct_Coaudition	
+  UNION ALL
+	SELECT C1.name1,C2.name2
+	FROM Coaudition C1, Direct_Coaudition C2
+	WHERE C1.name2 = C2.name1 AND C1.name1 != C2.name2 AND NOT EXISTS(
+		SELECT C.name1, C.name2
+		FROM Coaudition C
+		WHERE C.name1 = C1.name1 and C.name2 = C2.name2
+	)
+);
+SELECT * FROM Coaudition C WHERE C.name1<C.name2;
+	
