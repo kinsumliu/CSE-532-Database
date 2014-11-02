8787b84585b800a59e4c561c8159cd842eeeb9b7
Rey <rey123123.cs98g@g2.nctu.edu.tw>
2014-11-02 16:51:48 -0500

    Change view pos.

--- project2/tss.sql
+++ project2/tss.sql
@@ -1,21 +1,6 @@
 -- Query 1: Find all pairs of contestants who happened to audition the same piece
 -- during the same show and got the same score from at least one judge
 
-CREATE VIEW Direct_Coaudition AS (
-	SELECT C1.name AS name1, C2.name AS name2
-	FROM Contestants C1, Contestants C2,
-	  Show_Performances SP1, Show_Performances SP2,
-	  Performances P1, Performances P2
-	WHERE SP1.show = SP2.show AND
-	  SP1.performance = P1.oid AND
-	  SP2.performance = P2.oid AND
-	  P1.piece = P2.piece AND
-	  P1.contestant = C1.oid AND
-	  P2.contestant = C2.oid AND
-	  C1.name != C2.name AND
-	  P1.results && P2.results
-);
-
 SELECT * FROM Direct_Coaudition WHERE name1<name2;
 
 -- Query 2: Find all pairs of contestants who happened to audition the same piece
@@ -32,16 +17,6 @@ WHERE P1.piece = P2.piece AND
 
 -- Query 3: Find all pairs of contestants who auditioned the same piece in (possibly different) shows that
 -- had at least 3 judges and the two contestants got the same highest score.
-CREATE VIEW Shows3Judges AS
-  SELECT oid, showdate
-  FROM Shows S
-  WHERE EXISTS(
-    SELECT P.contestant
-    FROM Show_Performances SP, Performances P
-    WHERE S.oid = SP.show AND
-      SP.performance = P.oid AND
-      array_length(P.results, 1) > 2
-  );
 
 SELECT C1.name AS name1, C2.name AS name2
 FROM Contestants C1, Contestants C2,
@@ -95,16 +70,4 @@ WHERE
 -- X and Y (directly) co-auditioned iff they both performed the same piece in the
 -- same show and got the same score from at least one (same) judge.
 
-CREATE RECURSIVE VIEW Coaudition (name1, name2) AS (
-	SELECT *
-	FROM Direct_Coaudition
-  UNION ALL
-	SELECT C1.name1,C2.name2
-	FROM Coaudition C1, Direct_Coaudition C2
-	WHERE C1.name2 = C2.name1 AND C1.name1 != C2.name2 AND NOT EXISTS(
-		SELECT C.name1, C.name2
-		FROM Direct_Coaudition C
-		WHERE C.name1 = C1.name1 and C.name2 = C2.name2
-	)
-);
 SELECT * FROM Coaudition C WHERE C.name1<C.name2;
--- project2/tss_schema.sql
+++ project2/tss_schema.sql
@@ -54,3 +54,43 @@ CREATE TABLE Show_Performances (
 	show oid REFERENCES Shows(oid),
 	performance oid REFERENCES Performances(oid)
 );
+
+CREATE VIEW Shows3Judges AS
+  SELECT oid, showdate
+  FROM Shows S
+  WHERE EXISTS(
+    SELECT P.contestant
+    FROM Show_Performances SP, Performances P
+    WHERE S.oid = SP.show AND
+      SP.performance = P.oid AND
+      array_length(P.results, 1) > 2
+  );
+
+CREATE VIEW Direct_Coaudition AS (
+	SELECT C1.name AS name1, C2.name AS name2
+	FROM Contestants C1, Contestants C2,
+	  Show_Performances SP1, Show_Performances SP2,
+	  Performances P1, Performances P2
+	WHERE SP1.show = SP2.show AND
+	  SP1.performance = P1.oid AND
+	  SP2.performance = P2.oid AND
+	  P1.piece = P2.piece AND
+	  P1.contestant = C1.oid AND
+	  P2.contestant = C2.oid AND
+	  C1.name != C2.name AND
+	  P1.results && P2.results
+);
+
+
+CREATE RECURSIVE VIEW Coaudition (name1, name2) AS (
+	SELECT *
+	FROM Direct_Coaudition
+  UNION ALL
+	SELECT C1.name1,C2.name2
+	FROM Coaudition C1, Direct_Coaudition C2
+	WHERE C1.name2 = C2.name1 AND C1.name1 != C2.name2 AND NOT EXISTS(
+		SELECT C.name1, C.name2
+		FROM Direct_Coaudition C
+		WHERE C.name1 = C1.name1 and C.name2 = C2.name2
+	)
+);
